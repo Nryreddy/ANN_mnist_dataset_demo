bbc827505a6555bfc1fe352c82092a6201b01a1b
Nryreddy <nryreddy@gmail.com>
2021-10-09 19:29:15 +0530

    small tweaks in setup

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-PROJECT_NAME="ANN_mnist_dataset_demo"
-USER_NAME="Nryreddy"
 
-setuptools.setup(
-    name=f"{PROJECT_NAME}-{USER_NAME}",
+
+setup(
+    name="ANN_mnist_dataset_demo",
     version="0.0.1",
-    author={USER_NAME},
+    author="Nryreddy",
     description="A small packagefor ANN implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    url="https://github.com/Nryreddy/ANN_mnist_dataset_demo",
     project_urls={
-        "Bug Tracker": "https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+        "Bug Tracker": "https://github.com/Nryreddy/ANN_mnist_dataset_demo/issues",
     },
     
     packages=["src"],
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
     install_requires=[
         "tensorflow",
